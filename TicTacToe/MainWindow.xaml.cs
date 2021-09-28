d6876ff906f73f93312ca2ff8629a6df5b8538ed
Broadway1 <91154197+Broadway1@users.noreply.github.com>
2021-09-28 08:30:51 +0200

    Änderung

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -23,51 +23,6 @@ namespace TicTacToe
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void kaestchen_1_1_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void kaestchen_2_1_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void kaestchen_3_1_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Kaestchen_1_2_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Kaestchen_2_2_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Kaestchen_3_2_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Kaestchen_1_3_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Kaestchen_2_3_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Kaestchen_3_3_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+        }                
     }
 }
