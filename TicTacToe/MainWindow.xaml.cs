b88f5dab4b1bd33438205e3d2c380be86c42153f
Broadway1 <91154197+Broadway1@users.noreply.github.com>
2021-10-07 16:52:47 +0200

    Revert "System.NullReferenceException:"
    This reverts commit 70e08cc1fa2a21adb1355c1268da2d90cc993bc2.

---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void KaestchenClick(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_0_0.Foreground;
+            kaestchen_0_0.Foreground = kaestchen_0_0.Background;
+            kaestchen_0_0.Background = bisherigerVordergrund;
+        }
+
+        private void Kaestchen_1_0_Click(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_1_0.Foreground;
+            kaestchen_1_0.Foreground = kaestchen_1_0.Background;
+            kaestchen_1_0.Background = bisherigerVordergrund;
+        }
+
+        private void Kaestchen_2_0_Click(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_2_0.Foreground;
+            kaestchen_2_0.Foreground = kaestchen_2_0.Background;
+            kaestchen_2_0.Background = bisherigerVordergrund;
+        }
+
+        private void Kaestchen_0_1_Click(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_0_1.Foreground;
+            kaestchen_0_1.Foreground = kaestchen_0_1.Background;
+            kaestchen_0_1.Background = bisherigerVordergrund;
+        }
+
+        private void Kaestchen_1_1_Click(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_1_1.Foreground;
+            kaestchen_1_1.Foreground = kaestchen_1_1.Background;
+            kaestchen_1_1.Background = bisherigerVordergrund;
+        }
+
+        private void Kaestchen_2_1_Click(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_2_1.Foreground;
+            kaestchen_2_1.Foreground = kaestchen_2_1.Background;
+            kaestchen_2_1.Background = bisherigerVordergrund;
+        }
+
+        private void Kaestchen_0_2_Click(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_0_2.Foreground;
+            kaestchen_0_2.Foreground = kaestchen_0_2.Background;
+            kaestchen_0_2.Background = bisherigerVordergrund;
+        }
+
+        private void Kaestchen_1_2_Click(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_1_2.Foreground;
+            kaestchen_1_2.Foreground = kaestchen_1_2.Background;
+            kaestchen_1_2.Background = bisherigerVordergrund;
+        }
+
+        private void Kaestchen_2_2_Click(object sender, RoutedEventArgs e)
+        {
+            var bisherigerVordergrund = kaestchen_2_2.Foreground;
+            kaestchen_2_2.Foreground = kaestchen_2_2.Background;
+            kaestchen_2_2.Background = bisherigerVordergrund;
+        }
+    }
+}
